5b3d6a2c57243b01f119131875140fee9beea5bc
cgonzalezavalos <cgonzaleza@serviciocivil.cl>
2024-05-02 10:15:25 -0400

    comment matplotlib

--- APP_EFP.py
+++ APP_EFP.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 #import plotly.express as px
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 #%matplotlib inline
 import seaborn as sns
 
