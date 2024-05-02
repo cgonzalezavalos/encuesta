2e308fcc139cd5466fee3e4b778cb34868199df9
cgonzalezavalos <cgonzaleza@serviciocivil.cl>
2024-05-02 10:14:03 -0400

    comment ploty exppress

--- APP_EFP.py
+++ APP_EFP.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import plotly.express as px
+#import plotly.express as px
 import matplotlib.pyplot as plt
 #%matplotlib inline
 import seaborn as sns
