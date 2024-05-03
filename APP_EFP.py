26d323ac7ce786cab1629c4542f7ecc57a3ec647
cgonzalezavalos <cgonzaleza@serviciocivil.cl>
2024-05-03 12:22:46 -0400

    Mostrar los valores sobre las barras en graf1

--- APP_EFP.py
+++ APP_EFP.py
@@ -166,6 +166,8 @@ graf1=px.bar(df_promedios_todos,x='Indice',y='Resultado',title=f'<b>Resultados {
                  update_xaxes(title_text=None)
 graf1.update_layout(yaxis_tickformat='.0f',width=1000,  # Ancho del gráfico en píxeles
     height=800,)
+# Mostrar los valores sobre las barras
+graf1.update_traces(text=graf1.data[0]['y'], texttemplate='%{text:.0f}', textposition='outside')
 
 #------------------------------------------------------------------------
 # Definir un diccionario de colores para las categorías
