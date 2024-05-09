3a2aa5165a58fd515c9ded06547d0bd628d914b1
cgonzalezavalos <cgonzaleza@serviciocivil.cl>
2024-05-09 09:49:38 -0400

    cambio en filtros

--- APP_EFP.py
+++ APP_EFP.py
@@ -172,10 +172,12 @@ if option_1!='Todos' and option_2=='Todos':
     df_promedios_todos=df_promedios_todos.query(f"Sector=='{option_1}'")
 
 if option_1!='Todos' and option_2!='Todos':
-    df_promedios_servicios_todos=df_promedios_servicios_todos[df_promedios_servicios_todos(['Sector']=='{option_1}') & (df_promedios_servicios_todos['Servicio']=='{option_2}')]
+    #df_promedios_servicios_todos=df_promedios_servicios_todos[df_promedios_servicios_todos(['Sector']=='{option_1}') & (df_promedios_servicios_todos['Servicio']=='{option_2}')]
+    df_promedios_servicios_todos=df_promedios_servicios_todos[(df_promedios_servicios_todos['Servicio']=='{option_2}')]
 #     # data frame con resumen de indicadores
 #     df_resumen_indicaores=df_encuesta[(df_encuesta.Servicio==option_2) & (df_encuesta['Caracteristica de Comparacion']=='Todos') & (df_encuesta['Tipo']=='Indice')]
-# if option_1!='Todos' and option_2=='Todos':
+if option_1=='Todos' and option_2!='Todos':
+    df_promedios_servicios_todos=df_promedios_servicios_todos[(df_promedios_servicios_todos['Servicio']=='{option_2}')]
      
 
 with st.container():
