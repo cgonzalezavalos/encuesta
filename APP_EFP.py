7df8c3c80e9acc3624ca7ad2472feb4683fba93e
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2024-05-16 09:19:27 -0400

    Update garf1 version_graf=2.py
    se cambia el grafico 1 para que en la version 2 muestre 3 barras, AC/sector/servicio

--- APP_EFP.py
+++ APP_EFP.py
@@ -274,7 +274,8 @@ if version_grafico=='version_1':
         update_yaxes(visible=visible_y_axis,title_text=None).\
                  update_xaxes(title_text=None)
 if version_grafico=='version_2':
-    graf1=px.bar(df_promedios_servicios_todos,x='Indice',y='Resultado',title=f'<b>Resultados {option_2} por Indices</b>',color_discrete_map=dimension_colors).\
+    # graf1=px.bar(df_promedios_servicios_todos,x='Indice',y='Resultado',title=f'<b>Resultados {option_2} por Indices</b>',color_discrete_map=dimension_colors).\
+    graf1=px.bar(df_promedios_todos,x='Indice',y='Resultado',title=f'<b>Comparación de resultados por indices entre todos los sectores y {option_1}</b>',color='Sector', barmode='group',text='Resultado').\
         update_yaxes(visible=visible_y_axis,title_text=None).\
                  update_xaxes(title_text=None)
 if version_grafico=='version_3':
